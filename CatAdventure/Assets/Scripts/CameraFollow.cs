dd8180a51339e377bbe2957e139486f9e2b4b630
yesman <kaka08045176@gmail.com>
2022-04-10 14:58:53 +0900

    코드정리 및 변경사항( 카메라 cinemacine 사용) 커밋

--- CatAdventure/Assets/Scripts/CameraFollow.cs
+++ CatAdventure/Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float FollwSpeed = 2f;
+    public float FollowSpeed = 2f;
     public float yOffset = 1f;
     public Transform target;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPost = new Vector3(target.position.x, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPost, FollwSpeed*Time.deltaTime);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed*Time.deltaTime);
     }
 }
