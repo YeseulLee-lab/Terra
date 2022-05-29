640a1866613a03e63dbb87775fd5c9c62ac9da1d
yeseul <kaka0804@naver.com>
2022-05-30 01:02:25 +0900

    itemobject에 skill type 추가. skill type으로 사용할 버튼 구분할 예정

--- Assets/Scripts/Inventory/SkillItemObject.cs
+++ Assets/Scripts/Inventory/SkillItemObject.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SkillItemType
+{
+    Water,
+    Fire,
+    Light,
+}
+
 [CreateAssetMenu(fileName = "New Skill Item Object", menuName = "Inventory System/Items/Skill Item")]
 public class SkillItemObject : ItemObject
 {
+    public SkillItemType skillType;
     public void Awake()
     {
         type = ItemType.SkillItem;
