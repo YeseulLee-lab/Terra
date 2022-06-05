877829d1034165469807b55c7e8d5543061172e6
yeseul <kaka0804@naver.com>
2022-06-05 19:20:58 +0900

    체크포인트 플레이어 지나가면 움직임, pivot  변경

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private FallDetector fallDetector;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name == "player")
         {
             fallDetector.CheckPoint = gameObject.transform;
+            anim.SetTrigger("Move");
         }
 
     }
