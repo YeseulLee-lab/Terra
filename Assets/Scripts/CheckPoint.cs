d107e56f0cd7326dea10370f15a38d31166c2d03
yeseul <kaka0804@naver.com>
2022-08-06 16:03:00 +0900

    체크포인트 소리 1번만 나도록 수정

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -6,6 +6,7 @@ public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private FallDetector fallDetector;
     private Animator animator;
+    private bool isChecked = false;
 
     private void Start()
     {
@@ -14,11 +15,15 @@ public class CheckPoint : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        AudioManager.instance.PlaySound("stage_clear_01");
-        if (collision.gameObject.name == "player")
+        if(!isChecked)
         {
-            fallDetector.CheckPoint = gameObject.transform;
-            //animator.SetTrigger("Move");
-        }
+            AudioManager.instance.PlaySound("stage_clear_01");
+            if (collision.gameObject.name == "player")
+            {
+                fallDetector.CheckPoint = gameObject.transform;
+                //animator.SetTrigger("Move");
+            }
+            isChecked = true;
+        }        
     }
 }
