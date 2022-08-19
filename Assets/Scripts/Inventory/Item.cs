5c02f82252ac171528a1013192f8472f7de1708e
yeseul <kaka0804@naver.com>
2022-08-19 12:03:08 +0900

    added: remark in item.cs

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -7,8 +7,10 @@ public class Item : MonoBehaviour
     public SkillItemObject skillItemObject;
     public int amount = 1;
 
+    //플레이어가 아이템에 부딪히면 아이템 얻음
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //TODO yeseul:아이템 얻을시 나는 소리: 종류마다 다르게 해야함
         AudioManager.instance.PlaySound("item_01");
         if (collision.tag == "Player")
         {
@@ -21,6 +23,7 @@ public class Item : MonoBehaviour
         }        
     }
 
+    //게임을 종료하면 스킬아이템 amount가 0으로 변경됨
     private void OnApplicationQuit()
     {
         skillItemObject.amount = 0;
